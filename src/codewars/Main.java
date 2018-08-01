f8c5ff30e19fb78934165efe0e608489629427b4
Kyrylo_Prykhodko1 <Kyrylo_Prykhodko1@epam.com>
2018-08-01 10:56:25 +0300

    10

--- src/codewars/Main.java
+++ src/codewars/Main.java
@@ -3,6 +3,6 @@ package codewars;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!!9");
+        System.out.println("Hello World!!!10");
     }
 }
